ff7ddf71839e9b4a1cefe20997e09c95c80c2797
Reinhardt <contact@reinhello.tech>
2022-06-08 08:00:37 +0700

    Set bookmark channel to Text channel only

--- framework/lib/Commands/Interaction/FSetupBookmark.ts
+++ framework/lib/Commands/Interaction/FSetupBookmark.ts
@@ -1,4 +1,4 @@
-import { CommandInteraction, TextableChannel } from "eris";
+import { CommandInteraction, Constants, TextableChannel } from "eris";
 import { NekoChanClient } from "../../Client";
 import { RichEmbed } from "../../Util";
 
@@ -7,6 +7,15 @@ export function setupBookmarkInteraction(client: NekoChanClient, interaction: Co
 
     for (const option of interaction.data.options) {
         args[option.name] = (option as any).value as string;
+    }
+
+    const channel = interaction.member.guild.channels.get(args.bookmark);
+
+    if (channel.type !== Constants.ChannelTypes.GUILD_TEXT) {
+        return interaction.createMessage({
+            embeds: [new RichEmbed().setColor(0xFFC0BC).setDescription("Channel must be **Text** channel.")],
+            flags: 64
+        });
     }
 
     const embed = new RichEmbed()
