427206c4c802242e6cf0c64c8df1d3a865c4e4bc
Reinhardt <contact@reinhello.tech>
2022-06-11 15:47:06 +0700

    Update GEDebug.ts

--- framework/lib/Events/GEDebug.ts
+++ framework/lib/Events/GEDebug.ts
@@ -3,6 +3,6 @@ import { IEvent } from "./IEvent";
 export const event: IEvent = {
     name: "debug",
     run: async (client, message: string, shardID: number) => {
-        client.logger.log({ color: "#FFDD1C", message, subTitle: "NekoChanFramework::Events::Debug", title: `SHARD ${shardID ?? "N/A"}`, type: "DEBUG" });
+        client.logger.log({ colour: "#FFDD1C", message, subTitle: "NekoChanFramework::Events::Debug", title: `SHARD ${shardID ?? "N/A"}`, type: "DEBUG" });
     }
 };
